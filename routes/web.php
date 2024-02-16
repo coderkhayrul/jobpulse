a15b8f50a55a3cda2dc0343153b9f766bbbdbd79
Khayrul Islam Shanto <coderkhayrul@gmail.com>
2024-02-16 23:32:25 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('admin.layouts.app');
+});
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+
+// ADMIN ROUTE LIST ==========>
+// Route::prefix('admin')->name('admin.')->group(function () {
+
+//     Route::get('/', [AdminController::class, 'dashboard'])->name('dashboard');
+// });
+
+
+
+
+
+
+
+
+
+require __DIR__ . '/auth.php';
+// require __DIR__ . '/company.php';
+// require __DIR__ . '/candidate.php';

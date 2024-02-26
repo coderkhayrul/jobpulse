b5e8f3dadb4e9dfb45ab00ccfe7482566048ec29
Khayrul Islam Shanto <coderkhayrul@gmail.com>
2024-02-26 17:26:53 +0600

    Postion Feature Refactor

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -34,15 +34,10 @@
 
 
         <li>
-            <a href="javascript: void(0);" class="has-arrow waves-effect" aria-expanded="true">
+            <a href="{{ route('admin.position.index') }}" class="waves-effect" aria-expanded="false">
                 <i class="bx bx-user"></i>
-                <span key="t-user-managment">User Managment</span>
+                <span key="t-positions">Positions</span>
             </a>
-            <ul class="sub-menu mm-collapse" aria-expanded="false" style="">
-                <li><a href="" key="t-admin">Admin</a></li>
-                <li><a href="" key="t-trainer">Trainers</a></li>
-                <li><a href="" key="t-users">Users</a></li>
-            </ul>
         </li>
     </ul>
 </div>
